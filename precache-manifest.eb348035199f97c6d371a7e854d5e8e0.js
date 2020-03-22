ba97b29b075ce39f4f2cd7739fb23bf2503a4819
khan <khan.ngo@gmail.com>
2020-03-22 00:59:32 -0400

    Updates

---
+++ precache-manifest.eb348035199f97c6d371a7e854d5e8e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704c8089b94125e598881c6f358ed705",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "7b0504ead983b82ac73e",
+    "url": "/react_todo_list/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "367b090337998ced5bab",
+    "url": "/react_todo_list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b0504ead983b82ac73e",
+    "url": "/react_todo_list/static/js/2.c9bd8128.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo_list/static/js/2.c9bd8128.chunk.js.LICENSE"
+  },
+  {
+    "revision": "367b090337998ced5bab",
+    "url": "/react_todo_list/static/js/main.f3c31ecb.chunk.js"
+  },
+  {
+    "revision": "ecbfe7823d96a2270580",
+    "url": "/react_todo_list/static/js/runtime-main.ce1b4f22.js"
+  }
+]);
